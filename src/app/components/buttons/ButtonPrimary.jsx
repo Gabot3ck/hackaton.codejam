90a1eba3f6dc497d27b3068ecc70b2a46d6be84c
Gabot3ck <jesus.castilloc@outlook.com>
2024-07-24 16:28:32 -0400

    se agrega archivo txt con credenciales

--- src/app/components/buttons/ButtonPrimary.jsx
+++ src/app/components/buttons/ButtonPrimary.jsx
@@ -8,7 +8,7 @@ export const ButtonPrimary = ( { title = 'Registrar', isChecking } ) => {
     <button
       disabled= { isChecking }
       className={ `w-full  h-[40px]
-                text-sm sm:text-base text-light-500 font-medium
+                text-sm sm:text-base text-white font-medium
                 bg-primary-500  rounded-full 
                 hover:bg-primary-600 
                 transition-colors duration-300 ease-out` }
